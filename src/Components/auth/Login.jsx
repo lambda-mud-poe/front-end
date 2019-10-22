201c6603d4a33aecd2da2b45cc8da893ee921a11
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-22 09:02:13 +0100

    implement Login functionality

--- src/Components/auth/Login.jsx
+++ src/Components/auth/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 export default function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post(' https://mount-doom-mud.herokuapp.com/api/login/', {
+        username,
+        password
+      })
+      .then(res => setLoginSuccess(res.data))
+      .catch(err => setLoginError(err.response.data.non_field_errors[0]));
+  };
+
+  if (loginSuccess) {
+    return <Redirect to="/" />;
+  }
+
+  if (loginError) {
+    alert(loginError);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="Enter Username"
+          type="text"
+          required
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          placeholder="Enter Password"
+          type="password"
+          required
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
