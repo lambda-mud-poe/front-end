5eabef7fcb2e6183778fd5c5a9fe7ab75d3c6df6
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-22 09:18:45 +0100

    create register component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import GameArea from './components/views/GameArea';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
         <Route exact path="/" component={GameArea} />
       </Router>
     </div>
---
+++ src/Components/auth/Register.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
+export default function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [retistrationSuccess, setRegistrationSuccess] = useState(false);
+  const [registrationError, setRegistrationError] = useState(false);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post(' https://mount-doom-mud.herokuapp.com/api/registration/', {
+        username,
+        password1: password,
+        password2: confirmPassword
+      })
+      .then(res => {
+        localStorage.setItem('key', res.data.key);
+        setRegistrationSuccess(true);
+      })
+      .catch(
+        err => console.log(err.response.data)
+        // setRegistrationError(err.response.data.non_field_errors[0])
+      );
+  };
+
+  if (retistrationSuccess) {
+    return <Redirect to="/login" />;
+  }
+
+  if (registrationError) {
+    alert(registrationError);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="Enter Username"
+          type="text"
+          required
+          onChange={e => setUsername(e.target.value)}
+        />
+
+        <input
+          placeholder="Enter Password"
+          type="password"
+          required
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <input
+          placeholder="Confirm Password"
+          type="password"
+          required
+          onChange={e => setConfirmPassword(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
