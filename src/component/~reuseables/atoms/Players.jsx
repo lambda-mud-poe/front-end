a390a5675c3f6aba2f16a7e2e283944727a8b577
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-24 11:57:13 +0100

    fix movement

--- src/component/~reuseables/atoms/Players.jsx
+++ src/component/~reuseables/atoms/Players.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { forestGreen, red } from '../variables/colors';
-import { valera_round} from '../variables/font-famiy';
+import { valera_round } from '../variables/font-famiy';
 
 const StyledPlayers = styled.div`
   height: 200px;
@@ -13,11 +13,20 @@ const StyledPlayers = styled.div`
 
   h4 {
     color: ${forestGreen};
-    font-family: ${valera_round}
+    font-family: ${valera_round};
   }
 `;
 
 export default function Players({ players }) {
+  if (players.length === 0) {
+    return (
+      <StyledPlayers>
+        <h4>Other Players</h4>
+        <p>- No other player is present in room</p>
+      </StyledPlayers>
+    );
+  }
+
   return (
     <StyledPlayers>
       <h4>Other Players</h4>
