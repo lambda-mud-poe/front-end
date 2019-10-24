f96c51aa721bbc256fefcbd550df26885efa686a
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-24 05:03:57 +0100

    style map

--- src/Components/~reuseables/atoms/GameScreen.jsx
+++ src/Components/~reuseables/atoms/GameScreen.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { FlexibleXYPlot, LineSeries, MarkSeries } from "react-vis";
+import {
+  FlexibleXYPlot,
+  LineSeries,
+  MarkSeries,
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  PolygonSeries,
+} from "react-vis";
 import { ScreenStyle } from "../styles/DashBoardStyles";
 
 const GameScreen = ({ map }) => {
@@ -26,55 +35,56 @@ const GameScreen = ({ map }) => {
     }
   });
 
-
   // Create arrays to hold point coordinates and direction
   const roomCoordinate = [];
   const connectedRoom = [];
 
-  if (Object.keys(formatedCoordinates).length !== 0){
-
-  // Loop through each room in the map object
-  for (let room in formatedCoordinates) {
-    // Set data equal to the first element (x, y coordinates)
-    // in each room of the room_data object
-    let roomLocation = formatedCoordinates[room][0];
-    roomCoordinate.push(roomLocation);
-    for (let adjacentRooms in formatedCoordinates[room][1]) {
-        let point = formatedCoordinates[formatedCoordinates[room][1][adjacentRooms]] !== undefined? formatedCoordinates[formatedCoordinates[room][1][adjacentRooms]][0] : 'No way';
-      connectedRoom.push([
-        formatedCoordinates[room][0],
-        point
-      ]);
+  if (Object.keys(formatedCoordinates).length !== 0) {
+    // Loop through each room in the map object
+    for (let room in formatedCoordinates) {
+      // Set data equal to the first element (x, y coordinates)
+      // in each room of the room_data object
+      let roomLocation = formatedCoordinates[room][0];
+      roomCoordinate.push(roomLocation);
+      for (let adjacentRooms in formatedCoordinates[room][1]) {
+        let point =
+          formatedCoordinates[formatedCoordinates[room][1][adjacentRooms]] !==
+          undefined
+            ? formatedCoordinates[
+                formatedCoordinates[room][1][adjacentRooms]
+              ][0]
+            : "No way";
+        connectedRoom.push([formatedCoordinates[room][0], point]);
+      }
     }
   }
-  }
-
 
   console.log("-----linked---", connectedRoom);
-  
 
   return (
     <ScreenStyle>
       <FlexibleXYPlot width={600} height={600}>
-          {connectedRoom.map(link => (
-            <LineSeries
-              strokeWidth="2"
-              color="#FF0"
-              data={link}
-              key={Math.random() * 100}
-            />
-          ))}
-          <MarkSeries
-            // current={this.props.currentRoom}
-            highlight="#1b00ff"
-            strokeWidth={3}
-            opacity="1"
-            size="3"
-            color="#008000"
-            data={roomCoordinate}
-            style={{ cursor: "pointer" }}
+        {connectedRoom.map(link => (
+          <LineSeries
+            strokeWidth="2"
+            color="#FF0"
+            data={link}
+            key={Math.random() * 100}
           />
-        </FlexibleXYPlot>
+        ))}
+        <MarkSeries
+          // current={this.props.currentRoom}
+          highlight="#1b00ff"
+          strokeWidth={5}
+          opacity="1"
+          color="#008000"
+          data={roomCoordinate}
+          style={{ cursor: "pointer" }}
+        />
+        {/* <PolygonSeries
+    className="polygon-series-example"
+    data={roomCoordinate}/> */}
+      </FlexibleXYPlot>
     </ScreenStyle>
   );
 };
