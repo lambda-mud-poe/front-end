cacdc3355a66171dd833af2e02a6ff98519b5f75
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-24 18:39:32 +0100

    refactor styles

--- src/components/~reuseables/molecules/ChatBox.jsx
+++ src/components/~reuseables/molecules/ChatBox.jsx
@@ -5,27 +5,21 @@ import styled from 'styled-components';
 import { forestGreen } from '../variables/colors';
 
 const StyledInput = styled.input`
-  /* background: transparent; */
-  /* border: 5px solid ${forestGreen}; */
   color: white;
   font-weight: 500;
   background: transparent;
-  border: none;
+  border: 1px dotted ${forestGreen};
   color: white;
   outline: 0;
   padding: 1rem;
   width: 100%;
-
-  &:focus {
-      border: none !important;
-
-  }
+  margin-top: 1rem;
 `;
 
 const ChatBox = ({ name, value, onChange }) => {
   return (
     <StyledForm>
-        <button>Logout</button>
+      <button>Logout</button>
       <StyledInput name="message" placeholder="Enter chat here" />
     </StyledForm>
   );
