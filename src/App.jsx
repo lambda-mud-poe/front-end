7b9929a75ff0eb37a35d0446ea2fd8c61cbd7e76
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-24 12:18:46 +0100

    fix folder referencing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./component/auth/Login";
-import Register from "./component/auth/Register";
-import PrivateRoute from "./component/~reuseables/molecules/PrivateRoute";
-import GameArea from "./component/views/GameArea";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import PrivateRoute from "./components/~reuseables/molecules/PrivateRoute";
+import GameArea from "./components/views/GameArea";
 
 function App() {
   return (
